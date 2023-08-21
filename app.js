888403e807af5b99e58161fbe646632b66b3cee0
Nomanshabbir <135643492+Nomanshabbir1@users.noreply.github.com>
2023-08-21 08:38:32 -0700

    ASSIGNMENT 3 Q1-8 DONE

---
+++ app.js
@@ -0,0 +1,55 @@
+// Q#01
+
+// var empty = []
+
+// console.log(empty)
+
+// // Q#2
+
+// var empty2 = {}
+
+// console.log(empty2)
+
+// // Q# 3
+
+// var studentname =['noman', 'shabbir', 'hussain']
+
+// console.log(studentname)
+
+// // Q#4
+
+// var studrollnumber = [1,2,3,4,5,6]
+
+// console.log(studrollnumber)
+
+// // Q#5
+
+// var rightans =[ true, false]
+
+// console.log(rightans)
+
+// Q#6
+
+// var mixed =[ 'noman',1 , true, 'shabbir', 2, true]
+
+// console.log(mixed)
+
+// Q#7
+
+// document.write("<h1>Qualifications</h1>");
+
+// var Qualifications = ["SSC", "HSC", "BCS",
+//     "BS", "BCOM", "MS", "M. Phil.", "PhD"]
+
+// console.log(Qualifications)
+
+// Q #8
+
+var studname =["Score of Michael","Score of John", "Score of Tony"]
+
+var studnumber =["320", "230", "480"]
+
+
+  
+
+  
